c220d336d9a53deddc97bc2717871dacc5819640
keeratipong17 <keeratiponngsave@gmail.com>
2025-10-19 15:12:05 +0700

    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'slawneavswmxfjkbvtpz.supabase.co',
+        port: '',
+        pathname: '/test_bk/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
